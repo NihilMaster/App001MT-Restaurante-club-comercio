7a95192ea446ffa077f942316efe8767ceb08aee
MateoNihil <mateo.nihil@gmail.com>
2026-03-01 18:35:06 -0700

    Initial commit

---
+++ src/main/java/org/mt/restaurantcc/RestaurantCcApplication.java
@@ -0,0 +1,13 @@
+package org.mt.restaurantcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantCcApplication.class, args);
+    }
+
+}
